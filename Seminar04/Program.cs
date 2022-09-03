506bb464c27bbc2fcef553e83c90107084ea6f71
Natalia Gordeeva <ganya85@gmail.com>
2022-09-03 13:32:32 +0300

    4 seminar 1

--- Seminar04/Program.cs
+++ Seminar04/Program.cs
@@ -86,20 +86,49 @@
 //1 вариант студентов - два метода
 // 
 
+int[] array = new int[8]; //нулевой массив
+// заполняем массив
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index<lenght)
+    {
+        collection[index]=new Random().Next(0, 2);
+        index++;
+    }
+}
+//выводим массив
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+
+}
+FillArray(array);
+PrintArray(array);
+
+
+
 // 2 вариант препода - один метод
 
-int [] array = GetArray(8);
-Console.WriteLine(string.Join(",",array));
+// int [] array = GetArray(8);
+// Console.WriteLine(string.Join(",",array));
 
-int [] GetArray(int size);     //вызов масива
-{
-    int [] result = new int [size];
-    for(int i=0; i<size; i++)
-    {
-        result[i]=new Random().Next(0,2);
+// int [] GetArray(int size);     //вызов масива
+// {
+//     int [] result = new int [size];
+//     for(int i=0; i<size; i++)
+//     {
+//         result[i]=new Random().Next(0,2);
 
-    }
-    return result;
-}
+//     }
+//     return result;
+// }
 
    
