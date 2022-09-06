9ede9d1751cfdbdacd576c558994a05fdb1efe3a
Natalia Gordeeva <ganya85@gmail.com>
2022-09-06 15:04:59 +0300

    5 seminar

--- Seminar05/Program.cs
+++ Seminar05/Program.cs
@@ -53,16 +53,67 @@
 //Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
 // второй и предпоследний и тд. Результат запишем в новом массиве.[12345]->583
 
-int [] inputArray={1,2,3,4,5};
-// int index=inputArray.Length;
+// int [] inputArray={1,2,3,4,5};
 
-int [] resultArray=new int[inputArray.Length/2+inputArray.Length%2];
-for(int i=0;i<resultArray.Length;i++)
+// int [] resultArray=new int[inputArray.Length/2+inputArray.Length%2];
+// for(int i=0;i<resultArray.Length;i++)
+// {
+//     resultArray[i]=inputArray[i]*inputArray[inputArray.Length-1-i];
+//     if(i==inputArray.Length-1-i)
+//     {
+//         resultArray[i]=inputArray[i];
+//     }
+// }
+// Console.WriteLine(String.Join(",",resultArray));
+
+// Напишите программу замена элементов массива: положительные элементы замени на соответствующие отрицательные 
+// и наоборот. [-4,-8,8,2]->[4,8,-8,-2]
+
+// int [] array={-4,-8,8,2};
+// for (int i=0; i<array.Length; i++)
+// {
+//     array[i]*=-1;
+// }
+// Console.WriteLine("[{0}]",String.Join(",",array));
+
+// задача Написать программу, которая определяет присутствует ли заданное число в массиве.
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[size];
+// for (int i = 0; i < size; i++)
+// {
+//     array[i] = new Random().Next(-9, 10);
+// }
+// Console.WriteLine("[{0}]", String.Join(";", array));
+
+// Console.WriteLine("введите число =");
+// int num1=Convert.ToInt32(Console.ReadLine());
+// int count =0;
+// for (int i=0;i<size;i++)
+// {
+//     if(array[i]==num1)
+//     {
+//         count++;
+//     }
+// }
+// if(count>0)
+// {
+// Console.WriteLine($"число найдено: {count}");
+// }
+// else
+// {
+//     Console.WriteLine("чисел нет");
+// }
+
+// Написать программу, которая определяет присутствует ли заданное число в массиве (2 вариант)
+Console.WriteLine("введите число =");
+int num=Convert.ToInt32(Console.ReadLine());
+string result = "no";
+int [] array = {1, 2, 3, 5, 6};
+for (int i=0; i<array.Length; i++)
 {
-    resultArray[i]=inputArray[i]*inputArray[inputArray.Length-1-i];
-    if(i==inputArray.Length-1-i)
+    if(array[i]==num)
     {
-        resultArray[i]=inputArray[i];
+        result="yas";
     }
 }
-Console.WriteLine(String.Join(",",resultArray));
+Console.WriteLine(result);
